30b1e30d889584026db696763cc96455f21f85c3
joel1842 <joeldaniellake@gmail.com>
2021-09-04 20:19:19 -0230

    September 4, 2021

--- src/Track.js
+++ src/Track.js
@@ -16,4 +16,30 @@ const Track = () => {
     )
 }
 
+setTimeout(start, 1000);
+
+let i = 0;
+let lapNumber = 0;
+
+function start() {
+    setInterval(Car, 500);
+  }
+
+const Car = () => {
+
+    if (i < 10) {
+        i++;
+        console.log("car position =", i);
+    } else {
+        i = 0;
+        if (lapNumber < 3) {
+            lapNumber += 1;
+        } else {
+            console.log("Race has ended!");
+            lapNumber = 0;
+        }
+        console.log("lap =", lapNumber);
+    }
+}
+
 export default Track;
