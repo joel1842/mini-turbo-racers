6ee1307fb5d18b48166ef26cb316fa2cf71130e3
joel1842 <joeldaniellake@gmail.com>
2021-09-05 16:15:26 -0230

    September 5, 2021

--- src/Track.js
+++ src/Track.js
@@ -16,30 +16,53 @@ const Track = () => {
     )
 }
 
-setTimeout(start, 1000);
+
 
 let i = 0;
-let lapNumber = 0;
-
-function start() {
-    setInterval(Car, 500);
-  }
+let lapCount = 0;
 
 const Car = () => {
-
     if (i < 10) {
         i++;
-        console.log("car position =", i);
-    } else {
+        console.log("Car Position =", i);
+    } else if (i === 10) {
+        lapCounter();
         i = 0;
-        if (lapNumber < 3) {
-            lapNumber += 1;
-        } else {
-            console.log("Race has ended!");
-            lapNumber = 0;
-        }
-        console.log("lap =", lapNumber);
     }
 }
+
+let carInterval = setInterval(Car, 500);
+
+const lapCounter = () => {
+    if (i === 10) {
+        if (lapCount < 3) {
+            lapCount += 1;
+            console.log("Lap:", lapCount);
+        } else if (lapCount === 3) {
+            console.log("Race has ended!");
+            clearInterval(carInterval);
+        }
+    }
+}
+
+const supremeFuel = new Object();
+supremeFuel.boost = 1.25;
+supremeFuel.price = 50;
+
+let randomInteger = () => {
+    return Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+}
+
+const powerUpPosition = () => {
+    let randNum = randomInteger();
+    if (randNum === i) {
+        console.log("SUPREME FUEL COLLECTED!");
+        carInterval = setInterval(Car, 200);
+    }
+}
+
+let powerUpInterval = setInterval(powerUpPosition, 5000);
+
+console.log("random number:", randomInteger());
 
 export default Track;
